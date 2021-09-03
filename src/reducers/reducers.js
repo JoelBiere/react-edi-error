0a3eef63dc1ac4383cba0c6474ca45176655e330
JoelBiere <76972193+JoelBiere@users.noreply.github.com>
2021-09-03 16:07:03 -0500

    Metrics and Filters complete

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -12,7 +12,8 @@ const initialState = {
     detailsShown: false,
     sortedBy: {
         department: false,
-        date: false,
+        mostRecent: false,
+        leastRecent: false,
         errID: false,
         client: false,
         invoiceAmount: false
@@ -38,9 +39,9 @@ export function cardsReducer(state = initialState, action) {
         case (actions.CARD_REASSIGNED):
             return {
                 ...state,
-                errData: state.errData.map(obj => obj.errorID !== action.payload.idOfToReassign ? obj : {...obj, department: action.payload.newDepartment }),
-                displayedCard: {...state.displayedCard, department: action.payload.newDepartment },
-                errCardsShowing: state.errCardsShowing.map(obj => obj.errorID !== action.payload.idOfToReassign ? obj : {...obj, department: action.payload.newDepartment })
+                errData: state.errData.map(obj => obj.errorID !== action.payload.idOfToReassign ? obj : { ...obj, department: action.payload.newDepartment }),
+                displayedCard: { ...state.displayedCard, department: action.payload.newDepartment },
+                errCardsShowing: state.errCardsShowing.map(obj => obj.errorID !== action.payload.idOfToReassign ? obj : { ...obj, department: action.payload.newDepartment })
             }
 
         case (actions.DETAILS_TOGGLED):
@@ -51,14 +52,14 @@ export function cardsReducer(state = initialState, action) {
 
         case (actions.CARD_RESOLVED):
 
-            let updatedSet = state.errData.map(obj => obj.errorID !== action.payload.idOfResolved ? obj : {...obj, isResolved: true })
+            let updatedSet = state.errData.map(obj => obj.errorID !== action.payload.idOfResolved ? obj : { ...obj, isResolved: true })
 
             let showingCards;
             let displayedCard;
 
             if (state.includeResolved) {
-                showingCards = state.errCardsShowing.map(obj => obj.errorID !== action.payload.idOfResolved ? obj : {...obj, isResolved: true })
-                displayedCard = {...state.displayedCard, isResolved: true }
+                showingCards = state.errCardsShowing.map(obj => obj.errorID !== action.payload.idOfResolved ? obj : { ...obj, isResolved: true })
+                displayedCard = { ...state.displayedCard, isResolved: true }
             } else if (!state.includeResolved) {
                 showingCards = state.errCardsShowing.filter(obj => obj.errorID !== action.payload.idOfResolved)
                 displayedCard = undefined
@@ -120,22 +121,65 @@ export function cardsReducer(state = initialState, action) {
             return {
                 ...state,
                 includeResolved: action.payload.checked,
-                errCardsShowing: cardsShowing
+                errCardsShowing: cardsShowing,
+                sortedBy: {...state.initialState}
             }
 
 
-            //Sorting cases
+        //Sorting cases
         case (actions.SORTED_BY_DEPARTMENT):
-            const sortedByDepartment = state.errCardsShowing.sort( (a, b) => a.department > b.department ? 1 : a.department < b.department ? -1 : 0 ) 
-            
+            const sortedByDepartment = state.errCardsShowing.sort((a, b) => a.department > b.department ? 1 : a.department < b.department ? -1 : 0)
+
             return {
                 ...state,
                 errCardsShowing: sortedByDepartment.filter(obj => obj === obj),
-                sortedBy: {...state.sortedBy, department: true}
+                sortedBy: { ...initialState.sortedBy, department: true }
             }
+
+        case (actions.SORTED_BY_ERRORID):
+            const sortedByErrorID = state.errCardsShowing.sort((a, b) => a.errorID > b.errorID ? 1 : a.errorID < b.errorID ? -1 : 0)
+            return {
+                ...state,
+                errCardsShowing: sortedByErrorID.filter(obj => obj === obj),
+                sortedBy: { ...initialState.sortedBy, errID: true }
+            }
+
+        case (actions.SORTED_BY_INVOICEAMOUNT):
+            const sortedByInvoiceAmount = state.errCardsShowing.sort((a, b) => a.price < b.price ? 1 : a.price > b.price ? -1 : 0)
+            return {
+                ...state,
+                errCardsShowing: sortedByInvoiceAmount.filter(obj => obj === obj),
+                sortedBy: { ...initialState.sortedBy, invoiceAmount: true }
+            }
+        case (actions.SORTED_BY_MOST_RECENT):
+            const sortedByMostRecent = state.errCardsShowing.sort((a, b) => a.errorDate < b.errorDate ? 1 : a.errorDate > b.errorDate ? -1 : 0)
+            return {
+                ...state,
+                sortedBy: { ...initialState.sortedBy, mostRecent: true },
+                errCardsShowing: sortedByMostRecent.filter(obj => obj === obj)
+            }
+
+        case (actions.SORTED_BY_LEAST_RECENT):
+            const sortedByLeastRecent = state.errCardsShowing.sort((a, b) => a.errorDate > b.errorDate ? 1 : a.errorDate < b.errorDate ? -1 : 0)
+            return {
+                ...state,
+                sortedBy: { ...initialState.sortedBy, leastRecent: true },
+                errCardsShowing: sortedByLeastRecent.filter(obj => obj === obj)
+            }
+
+        case (actions.SORTED_BY_CLIENT):
+            const sortedByClient = state.errCardsShowing.sort((a, b) => a.customer > b.customer ? 1 : a.customer < b.customer ? -1 : 0)
+            return {
+                ...state,
+                sortedBy: { ...initialState.sortedBy, client: true },
+                errCardsShowing: sortedByClient.filter(obj => obj === obj)
+            }
+
         default:
             return state;
     }
+
+
 }
 
 
